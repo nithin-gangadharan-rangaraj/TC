9b1819d82496e8ba21659cc0eab30e04da8c9324
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-03-02 20:38:47 +1100

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd 
+
+st.set_page_config(page_title="Candidate.ai")
+st.header("Candidate.ai", divider = 'red')
+
+# Run the app
+if __name__ == "__main__":
+    st.write("Welcome!")
