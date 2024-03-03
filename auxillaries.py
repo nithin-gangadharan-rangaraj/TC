cef2c1991d803b45c7d4049a6111937740dc21f0
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-03-03 11:41:14 +1100

    Create auxillaries.py

---
+++ auxillaries.py
@@ -0,0 +1,25 @@
+from google.oauth2 import service_account
+
+def initiate():
+  if 'gsheet' in st.session_state:
+        gsheet = st.session_state['gsheet']
+  else:
+      # Load service account credential
+      try:
+          service_acc = st.secrets["gcp_service_account"]
+      except KeyError:
+          service_acc = dict(eval(os.environ.get("gcp_service_account")))
+          
+      credentials = service_account.Credentials.from_service_account_info(
+      service_acc,
+      scopes=[
+          "https://www.googleapis.com/auth/spreadsheets",
+          "https://www.googleapis.com/auth/drive"
+      ],
+      )
+  
+      # Authorize gspread client
+      gs = gspread.authorize(credentials)
+      gsheet = gs.open("DCW")
+      st.session_state['gsheet'] = gsheet
+  return gsheet
