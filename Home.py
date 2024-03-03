22fbcf9dbe8e45007156b16d4e76753c4a9a40dc
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-03-03 11:03:39 +1100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,9 +1,26 @@
 import streamlit as st
 import pandas as pd 
+from openai import OpenAI
 
 st.set_page_config(page_title="Candidate.ai")
 st.header("Candidate.ai", divider = 'red')
 
+def main():
+    client = OpenAI()
+     client = OpenAI(
+                      api_key=st.secrets['keys']['OPENAI-API'],
+                    )
+    completion = client.chat.completions.create(
+                      model="gpt-3.5-turbo",
+                      messages=[
+                        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+                        {"role": "user", "content": "Compose a 10 word poem that explains the concept of recursion in programming."}
+                      ]
+                    )
+
+    st.write(completion.choices[0].message)
+    
 # Run the app
 if __name__ == "__main__":
     st.write("Welcome!")
+    main()
