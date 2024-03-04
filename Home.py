5c3d5552c8ba58538ec6c8811643c922650d51ef
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-03-04 14:33:17 +1100

    Update Home.py

--- Home.py
+++ Home.py
@@ -132,7 +132,7 @@ def update_df(candidate_df, emails):
     for email_info in emails:
         st.dataframe(candidate_df)
         if email_info['ID'] in candidate_df['ID'].values:
-            st.write(pd.series(email_info))
+            st.write(pd.Series(email_info))
             candidate_df.loc[candidate_df['ID'] == email_info['ID']] = pd.Series(email_info) #candidate_df.loc[candidate_df['ID'] == email_info['ID']].apply(detect_exchanges, args=(email_info,),  axis=1) = pd.Series(email_info) #candidate_df.loc[candidate_df['ID'] == email_info['ID']].apply(detect_exchanges, args=(email_info,),  axis=1)
         else:
             candidate_df = add_row(candidate_df, email_info)
