1b229355082fa9711350bb12abae5df2c095b68b
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-03-06 07:13:50 +1100

    Update 1_Create_Header.py

--- pages/1_Create_Header.py
+++ pages/1_Create_Header.py
@@ -1,7 +1,18 @@
 import streamlit as st
 from auxillaries import *
+from gspread_dataframe import get_as_dataframe
+
+def get_recruiter_df(wsheet):
+  return get_as_dataframe(wsheet)
+
+def get_inputs():
+  name = st.text_input("Enter the Firm Name")
+  title = st.text_input("Enter the Job Title")
+  job_description = st.text_area("Paste the job description")
+  
 
 if __name__ == "__main__":
   gsheet = initiate()
   wsheet = open_worksheet(gsheet, "Recruiters")
+  recruiter_df = get_recruiter_df(wsheet)
   st.header("Create a new header")
