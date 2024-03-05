d48f876ea939ec7f5ebaad9738b25a38cf1948e6
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-03-06 07:02:47 +1100

    Update 1_Create_Header.py

--- pages/1_Create_Header.py
+++ pages/1_Create_Header.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from auxillaries import initiate
+from auxillaries import *
 
 if __name__ == "__main__":
   gsheet = initiate()
+  wsheet = open_worksheet(gsheet, "Recruiters")
   st.header("Create a new header")
