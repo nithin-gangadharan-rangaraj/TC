9ec374b80961841bc35e51e582b06909717fe0dc
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-03-07 17:27:16 +1100

    Update email_auxillaries.py

--- email_auxillaries.py
+++ email_auxillaries.py
@@ -5,25 +5,28 @@ from email.mime.text import MIMEText
 import smtplib
 
 def send_report(df, recruiter):
-    multipart = MIMEMultipart()
-    multipart["From"] = st.secrets['email']
-    multipart["To"] = recruiter['Email']
-    multipart["Subject"] = f'Candidate.ai - {recruiter["Header"]} Report'  
-
-    message = """\
-    <p><strong>Candidate Report:</strong></p>
-    <p>Greetings,</p>
-    <p>Please find attached the candidate file for the job</p>
-    <p><strong>Regards,</strong><br><strong>Candidate.ai&nbsp;    </strong></p>
-    """
+    try:
+        multipart = MIMEMultipart()
+        multipart["From"] = st.secrets['email']
+        multipart["To"] = recruiter['Email']
+        multipart["Subject"] = f'Candidate.ai - {recruiter["Header"]} Report'  
     
-    attachment = MIMEApplication(df.to_csv())
-    attachment["Content-Disposition"] = 'attachment; filename=" {}"'.format(f"{recruiter['Header']}.csv")
-    multipart.attach(attachment)
-    multipart.attach(MIMEText(message, "html"))
-    
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(multipart["From"], st.secrets['password'])
-    server.sendmail(multipart["From"], multipart["To"], multipart.as_string())
-    server.quit()
+        message = """\
+        <p>Greetings,</p>
+        <p>Please find attached the candidate recommendations for your recruitment.</p>
+        <p><strong>Regards,</strong><br><strong>Candidate.ai&nbsp;    </strong></p>
+        """
+        
+        attachment = MIMEApplication(df.to_csv())
+        attachment["Content-Disposition"] = 'attachment; filename=" {}"'.format(f"{recruiter['Header']}.csv")
+        multipart.attach(attachment)
+        multipart.attach(MIMEText(message, "html"))
+        
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(multipart["From"], st.secrets['password'])
+        server.sendmail(multipart["From"], multipart["To"], multipart.as_string())
+        server.quit()
+        st.success(f"Email sent to {recruiter['Email'] successfully.}")
+    except:
+        st.error("Error. Please try again later.")
