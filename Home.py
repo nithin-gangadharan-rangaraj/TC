d64222c67e86d0f51d9e360046e661ee2d519412
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-03-07 11:29:26 +1100

    Update Home.py

--- Home.py
+++ Home.py
@@ -244,7 +244,7 @@ def generate_prompt(candidate):
     prompt = ""
     for column, value in candidate.items():
         if len(value) > 0:
-            prompt += ("\n" + f"{column.upper().strip()}" + "\n" + f"{value.strip()}")
+            prompt += (f"\n{column.upper().strip()}\n{value.strip()}")
     st.write(prompt)
     return prompt
 
