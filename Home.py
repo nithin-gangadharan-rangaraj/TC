2512ee871c4a863387c26da53347f9946df320c3
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-03-08 12:59:23 +1100

    Update Home.py

--- Home.py
+++ Home.py
@@ -325,7 +325,7 @@ def arrange_df(ranked_candidates, rec_df):
 
 
 def rank_using_ai(rec_df, recruiter, client):
-    all_candidates = '\n'.join([candidate['ID'] + "\n" + candidate['Recommendation'] for index, candidate in rec_df])
+    all_candidates = '\n'.join([candidate['ID'] + "\n" + candidate['Recommendation'] for index, candidate in rec_df.iterrows()])
     ranked_candidates = get_ai_help(client, all_candidates, recruiter)
     rec_df = arrange_df(ranked_candidates, rec_df)
     return rec_df
