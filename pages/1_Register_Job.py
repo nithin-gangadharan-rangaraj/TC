bec413cb82121246326442aae8239098b55b4ff5
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-03-19 20:07:15 +1100

    Update 1_Register_Job.py

--- pages/1_Register_Job.py
+++ pages/1_Register_Job.py
@@ -39,7 +39,7 @@ def get_inputs(df):
       inputs["Email"] = st.text_input("Enter the Email address").strip()
       inputs["JobDescription"] = st.text_area("Paste the job description").strip()
       inputs["FirmWebsite"] = st.text_input("Paste the link to the hiring firm's website").strip()
-      inputs["RankingParams"] = st.multiselect("Do you want us to consider specific parameters to rank the applicants?",
+      inputs["RankingParameters"] = st.multiselect("Do you want us to consider specific parameters to rank the applicants?",
                                                 options = get_ranking_params(),
                                                 help = "These parameters would be considered first when ranking the applicants.",
                                                 placeholder = "May choose upto 5 parameters",
