68120cd14f241f6fbae9c4feded81443bd780c06
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2024-06-12 16:33:37 +1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -356,9 +356,9 @@ def get_recommendation_ai(client, candidate, recruiter, scraped_candidate_conten
                           model="gpt-3.5-turbo",
                           messages=[
                             {"role": "system", "content": f'''You are a recruiter now. Analyse how good the candidate information fits the following recruiting job description.
-                                                            {recruiter['JobDescription']}. {("RECRUITING JOB WEBSITE: " + scraped_recruiter_content) if len(scraped_recruiter_content) > 10 else ''}
+                                                            Job description: {recruiter['JobDescription']}. {("RECRUITING JOB WEBSITE: " + scraped_recruiter_content) if len(scraped_recruiter_content) > 10 else ''} ####
                                                            You have to provide a recommendation for the user in less than 100 words. Strategically combine the personality traits, if any in information given in the candidate information.
-                                                           You Must Consider only the provided CANDIDATE INFORMATION.
+                                                           The job description is demilited by ####. You Must Consider only the provided CANDIDATE INFORMATION.
                                                            You Must answer it in the following format: 
                                                            Recommendation:
                                                            Personality traits:
